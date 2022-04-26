869d082816e97bb6622da49a8c638c456adc8188
Elmar5803 <lantz.mitch@gmail.com>
2022-04-26 10:01:51 -0500

    commited

--- lesson_3/loops_2_3.rb
+++ lesson_3/loops_2_3.rb
@@ -1,9 +1,10 @@
 process_the_loop = [true, false].sample
 
-for each process in process_the_loop
-  if process == true
+if process_the_loop
+  loop do
     puts "The loop was processed"
-  else 
-    "The loop wasn't processed"
+    break
   end
+else
+   puts "The loop wasn't processed"
 end
