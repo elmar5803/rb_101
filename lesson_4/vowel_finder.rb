4f3ee4e1b65758b64e75bf2b8a00c55cec9cbd7d
Elmar5803 <lantz.mitch@gmail.com>
2022-04-30 18:54:09 -0500

    Added new folder

---
+++ lesson_4/vowel_finder.rb
@@ -0,0 +1,21 @@
+def select_vowels(str)
+  selected_chars = ''
+  counter = 0
+
+  loop do
+    current_char = str[counter]
+
+    if 'aeiouAEIOU'.include?(current_char)
+      selected_chars << current_char
+    end
+
+    counter += 1
+    break if counter == str.size
+  end
+
+  selected_chars
+end
+
+string = "This is a string that I wrote"
+
+p number_vowels = select_vowels(string).size
